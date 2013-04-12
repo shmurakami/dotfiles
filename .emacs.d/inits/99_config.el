d6c175753f79b40a01be0a3d108f8fe46eb3926e
shmurakami <shmurakami1104@gmail.com>
2013-04-13 00:19:03 +0900

    revert

---
+++ .emacs.d/inits/99_config.el
@@ -0,0 +1,5 @@
+;; set encoding to utf-8-unix
+;; 00で設定してたけどinitsのどこかで変わってしまうので99で最後に設定する
+(set-default-coding-systems 'utf-8-unix)
+(when (eq system-type 'windows-nt)
+  (set-w32-system-coding-system 'utf-8-unix)) ; for windows
