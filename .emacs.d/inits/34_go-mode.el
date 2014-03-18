54e6ac9f3f8ea82a46e9a4dc889c9f0e46efac1b
Murakami.Shunsuke <murakami.shunsuke@aainc.co.jp>
2014-03-18 22:11:02 +0900

    もろもろ

--- .emacs.d/inits/34_go-mode.el
+++ .emacs.d/inits/34_go-mode.el
@@ -1,21 +1,36 @@
-;; enable
+;; for go-mode
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp/golang"))
+(setenv "GOROOT" "/usr/local/go")
+(setenv "GOPATH" "/usr/local/go/lib")
 (require 'go-mode)
+
+;; go bin
+(add-to-list 'exec-path (expand-file-name (getenv "GOROOT")))
+
+;; go get tools
+(add-to-list 'exec-path (expand-file-name (getenv "GOPATH")))
+
+;; gofmt before save
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+;; godef jump
+;(add-hook 'go-mode-hook (lamda ()
+;							   (local-set-key (kbd "M-.") 'godef-jump)))
 
 (defun go-mode-hooks ()
   "go-mode hooks"
 )
 (add-hook 'go-mode 'go-mode-hooks)
 
-;; go bin
-(add-to-list 'exec-path (expand-file-name "/usr/local/go/bin"))
-
-;; go get tools
-(add-to-list 'exec-path (expand-file-name "/usr/local/go/lib/bin"))
-
-;; gofmt before save
-(add-hook 'before-save-hook 'gofmt-before-save))
-
-;; godef jump
-(add-hook 'go-mode-hook (lamda ()
-							   (local-set-key (kbd "M-.") 'godef-jump)))
-
+(eval-after-load "go-mode"
+  '(progn
+	 (require 'go-autocomplete)
+	 (require 'auto-complete-config)
+;	 (require 'go-flymake)
+;	 (require 'go-flycheck)
+	 (require 'go-eldoc)
+	 (require 'golint)
+     (add-hook 'go-mode-hooks 'go-eldocs-setup)
+	 ;; key binding
+	 (define-key go-mode-map (kbd "M-.") 'godef-jump)
+	 (define-key go-mode-map (kbd "M-,") 'pop-tag-mark)))
